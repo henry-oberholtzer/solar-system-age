62a233c1826032d64581fd534d2cda82b1568b8f
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-10 09:23:03 -0800

    second test is passing GREEN phase

--- src/js/planetaryage.js
+++ src/js/planetaryage.js
@@ -4,5 +4,14 @@ export default class PlanetaryAge{
     }
 
     earthDays() {
+        const birthday = new Date(this.dob[2], (this.dob[0] - 1), this.dob[1]);
+        console.log(birthday)
+        const nowMS = Date.now()
+        console.log(nowMS)
+        const birthdayMS = birthday[Symbol.toPrimitive]('number');
+        console.log(birthdayMS);
+        const days = Math.round((nowMS - birthdayMS)/(1000 * 60 * 60 * 24));
+        this.earthAgeDays = days;
+        return days;
     }
 }
