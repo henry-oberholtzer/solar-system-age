1b67c0e1d076238688dd15fe1f48d9a2b05ebd8d
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-10 08:42:28 -0800

    First test RED phase (fixed bad fail)

--- __tests__/planetaryage.test.js
+++ __tests__/planetaryage.test.js
@@ -2,7 +2,7 @@ import PlanetaryAge from "../src/js/planetaryage.js";
 
 describe("PlanetaryAge", () => {
     test("Should create an object that accepts and stores a day, month and year of birth from a user", () => {
-        const planetary = PlanetaryAge(6, 27, 1998);
+        const planetary = new PlanetaryAge(6, 27, 1998);
         expect(planetary.dob).toEqual([6, 27, 1998]);
     });
 });
